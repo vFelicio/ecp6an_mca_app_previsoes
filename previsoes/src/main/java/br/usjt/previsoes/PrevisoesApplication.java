9ddfb24cc279d0cf7a806260faa3ff22414c8bdc
user <v.felicio@hotmail.com>
2020-05-07 18:56:17 -0300

    Versão inicial

---
+++ previsoes/src/main/java/br/usjt/previsoes/PrevisoesApplication.java
@@ -0,0 +1,13 @@
+package br.usjt.previsoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevisoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevisoesApplication.class, args);
+	}
+
+}
---
+++ previsoes/src/main/java/br/usjt/previsoes/model/Previsoes.java
@@ -0,0 +1,58 @@
+package br.usjt.previsoes.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Previsoes implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id; 
+	private String diaSemana;
+	private Double tempMin;
+	private Double tempMax;
+	private Double umidade;
+	private String descricao;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getdiaSemana() {
+		return diaSemana;
+	}
+	public void setNome(String diaSemana) {
+		this.diaSemana = diaSemana;
+	}
+	public Double getTempMin() {
+		return tempMin;
+	}
+	public void setTempMin(Double tempMin) {
+		this.tempMin = tempMin;
+	}
+	public Double getTempMax() {
+		return tempMax;
+	}
+	public void setTempMax(Double tempMax) {
+		this.tempMax = tempMax;
+	}
+	public Double getUmidade() {
+		return umidade;
+	}
+	public void setUmidade(Double umidade) {
+		this.umidade = umidade;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
